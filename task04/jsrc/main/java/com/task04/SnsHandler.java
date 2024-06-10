c913b4c433650abfdc4af7ac6d83a103a2ffa83e
Maksim_Elyashevich <maximelyashevich@gmail.com>
2024-06-10 12:06:49 +0300

    AWS Lambdas & SQS + SNS Integration

---
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -0,0 +1,26 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.RegionScope;
+import com.syndicate.deployment.model.ResourceType;
+import com.syndicate.deployment.model.RetentionSetting;
+
+
+@LambdaHandler(lambdaName = "sns_handler",
+        roleName = "sns_handler-role",
+        isPublishVersion = false,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SnsEventSource(targetTopic = "lambda_topic", regionScope = RegionScope.DEFAULT)
+@DependsOn(name = "async_queue", resourceType = ResourceType.SNS_TOPIC)
+public class SnsHandler implements RequestHandler<Object, String> {
+
+    public String handleRequest(Object request, Context context) {
+        System.out.println(request.toString());
+        return "Successfully logged even data from SNS";
+    }
+}
