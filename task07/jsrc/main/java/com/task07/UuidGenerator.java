718f1b972b10cf49cbb24d51eb081bb5d7acfac7
Maksim_Elyashevich <maximelyashevich@gmail.com>
2024-06-12 00:44:38 +0300

    Add CloudWatch and S3 integration

--- task07/jsrc/main/java/com/task07/UuidGenerator.java
+++ task07/jsrc/main/java/com/task07/UuidGenerator.java
@@ -2,25 +2,58 @@ package com.task07;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
+import com.syndicate.deployment.annotations.events.RuleEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.model.RetentionSetting;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-@LambdaHandler(lambdaName = "uuid_generator",
-	roleName = "uuid_generator-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
-)
-public class UuidGenerator implements RequestHandler<Object, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
-	}
+@LambdaHandler(lambdaName = "uuid_generator", roleName = "uuid_generator-role")
+@RuleEventSource(targetRule = "uuid_trigger")
+@EnvironmentVariables(value = {
+        @EnvironmentVariable(key = "region", value = "${region}"),
+        @EnvironmentVariable(key = "target_bucket", value = "${target_bucket}")
+})
+public class UuidGenerator implements RequestHandler<Object, String> {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public String handleRequest(Object request, Context context) {
+
+        var uuids = IntStream.range(0, 10)
+                .mapToObj(i -> UUID.randomUUID().toString())
+                .collect(Collectors.toList());
+
+        var data = new HashMap<String, Object>();
+        data.put("ids", uuids);
+
+        try (var s3Client = S3Client.builder().build()) {
+            var fileName = DateTimeFormatter.ISO_INSTANT.format(Instant.now());
+            var content = objectMapper.writeValueAsString(data);
+
+            var putRequest = PutObjectRequest.builder()
+                    .bucket(System.getenv("target_bucket"))
+                    .key(fileName)
+                    .build();
+
+            s3Client.putObject(putRequest, RequestBody.fromBytes(content.getBytes(StandardCharsets.UTF_8)));
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException(e);
+        }
+
+        return "Completed";
+    }
 }
