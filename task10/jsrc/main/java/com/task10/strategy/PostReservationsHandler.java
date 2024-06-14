df0775c0ddfbd3d56d8292fd28efbd1619d1dcbb
Maksim_Elyashevich <maximelyashevich@gmail.com>
2024-06-14 19:38:08 +0300

    Add Cognito integration

--- task10/jsrc/main/java/com/task10/strategy/PostReservationsHandler.java
+++ task10/jsrc/main/java/com/task10/strategy/PostReservationsHandler.java
@@ -9,7 +9,10 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import org.apache.http.HttpStatus;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 
 public class PostReservationsHandler extends AbstractDBHandler {
@@ -28,25 +31,32 @@ public class PostReservationsHandler extends AbstractDBHandler {
             );
             final int tableNumber = Integer.parseInt(input.get("tableNumber"));
 
-            if (isTableExisting(tableNumber)) {
-                String reservationId = String.valueOf(UUID.randomUUID());
-
-                var reservationItem = new HashMap<String, AttributeValue>();
-                reservationItem.put("id", new AttributeValue(reservationId));
-                reservationItem.put("tableNumber", new AttributeValue().withN(String.valueOf(tableNumber)));
-                reservationItem.put("clientName", new AttributeValue().withS(String.valueOf(input.get("clientName"))));
-                reservationItem.put("phoneNumber", new AttributeValue().withS(String.valueOf(input.get("phoneNumber"))));
-                reservationItem.put("date", new AttributeValue().withS(String.valueOf(input.get("date"))));
-                reservationItem.put("slotTimeStart", new AttributeValue().withS(String.valueOf(input.get("slotTimeStart"))));
-                reservationItem.put("slotTimeEnd", new AttributeValue().withS(String.valueOf(input.get("slotTimeEnd"))));
-
-                dynamoDB.putItem(System.getenv("reservations_table"), reservationItem);
-                return new APIGatewayProxyResponseEvent()
-                        .withStatusCode(HttpStatus.SC_OK)
-                        .withBody(String.format("{\"reservationId\": \"%s\"}", reservationId));
-            } else {
+            if (!isTableExisting(tableNumber)) {
                 throw new IllegalStateException(String.format("Table number '%s' not found.", tableNumber));
             }
+
+            var reservations = getExistingReservations(tableNumber);
+            for (var existingReservation : reservations) {
+                if (hasOverlap(input, existingReservation)) {
+                    throw new IllegalStateException("Conflicting reservation");
+                }
+            }
+
+            var reservationId = String.valueOf(UUID.randomUUID());
+
+            var reservationItem = new HashMap<String, AttributeValue>();
+            reservationItem.put("id", new AttributeValue(reservationId));
+            reservationItem.put("tableNumber", new AttributeValue().withN(String.valueOf(tableNumber)));
+            reservationItem.put("clientName", new AttributeValue().withS(String.valueOf(input.get("clientName"))));
+            reservationItem.put("phoneNumber", new AttributeValue().withS(String.valueOf(input.get("phoneNumber"))));
+            reservationItem.put("date", new AttributeValue().withS(String.valueOf(input.get("date"))));
+            reservationItem.put("slotTimeStart", new AttributeValue().withS(String.valueOf(input.get("slotTimeStart"))));
+            reservationItem.put("slotTimeEnd", new AttributeValue().withS(String.valueOf(input.get("slotTimeEnd"))));
+
+            dynamoDB.putItem(System.getenv("reservations_table"), reservationItem);
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(HttpStatus.SC_OK)
+                    .withBody(String.format("{\"reservationId\": \"%s\"}", reservationId));
 
         } catch (IllegalStateException e) {
             return new APIGatewayProxyResponseEvent()
@@ -67,5 +77,23 @@ public class PostReservationsHandler extends AbstractDBHandler {
         return scanResult.getItems()
                 .stream()
                 .anyMatch(item -> item.containsKey("number") && (item.get("number").getN().equals(tableNumberString)));
+    }
+
+    private List<Map<String, AttributeValue>> getExistingReservations(final int tableNumber) {
+        var tableNumberString = String.valueOf(tableNumber);
+        var scanRequest = new ScanRequest().withTableName(System.getenv("reservations_table"));
+        var scanResult = dynamoDB.scan(scanRequest);
+
+        return scanResult.getItems()
+                .stream()
+                .filter(item -> item.containsKey("tableNumber") && (item.get("tableNumber").getN().equals(tableNumberString)))
+                .collect(Collectors.toList());
+    }
+
+    private boolean hasOverlap(Map<String, String> newReservation, Map<String, AttributeValue> existingReservation) {
+        return newReservation.get("tableNumber").equals(existingReservation.get("tableNumber").getN())
+                && newReservation.get("date").equals(existingReservation.get("date").getS())
+                && (newReservation.get("slotTimeStart").compareTo(existingReservation.get("slotTimeStart").getS()) <= 0
+                && newReservation.get("slotTimeEnd").compareTo(existingReservation.get("slotTimeEnd").getS()) >= 0);
     }
 }
